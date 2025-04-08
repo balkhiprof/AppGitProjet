f16e29d909698cf55c1fa07fa3537962b1dd78b1
Balkhi <balkhi.prof@gmail.com>
2025-04-08 10:58:08 +0100

    Depôt version 2

--- src/firstpackage/Employe.java
+++ src/firstpackage/Employe.java
@@ -8,6 +8,49 @@ package firstpackage;
  *
  * @author Balkhi
  */
-public class Employe {
+public class Employe extends Personne{
+    private String statut;
+    private double salaire,commission;
+    //Constructeurs
+
+    public Employe(String statut, String nom, String prenom, String tel, double salaire, double commission) {
+        super(nom, prenom, tel);
+        this.statut = statut;
+        this.salaire = salaire;
+        this.commission = commission;
+    }
+
+    public Employe(String statut, String nom, String prenom, String tel, String email, double salaire, double commission) {
+        super(nom, prenom, tel, email);
+        this.statut = statut;
+        this.salaire = salaire;
+        this.commission = commission;
+    }
+    //Getters and setters
+
+    public String getStatut() {
+        return statut;
+    }
+
+    public void setStatut(String statut) {
+        this.statut = statut;
+    }
+
+    public double getSalaire() {
+        return salaire;
+    }
+
+    public void setSalaire(double salaire) {
+        this.salaire = salaire;
+    }
+
+    public double getCommission() {
+        return commission;
+    }
+
+    public void setCommission(double commission) {
+        this.commission = commission;
+    }
+    
     
 }
--- src/firstpackage/Personne.java
+++ src/firstpackage/Personne.java
@@ -9,5 +9,56 @@ package firstpackage;
  * @author Balkhi
  */
 public class Personne {
+    //Les variables d'intances
+    protected String nom,prenom,tel,email;
+    //Constructeurs
+
+    public Personne(String nom, String prenom, String tel) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.tel = tel;
+    }
+
+    public Personne(String nom, String prenom, String tel, String email) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.tel = tel;
+        this.email = email;
+    }
+    //Getters and setters
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
     
 }
